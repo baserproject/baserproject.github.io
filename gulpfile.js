aeb4844c50635be6838de2503e29958d74fcf9b6
ryuring <ryuring55@gmail.com>
2021-12-18 16:10:56 +0900

    gulp-cached プラグインを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var puml = require('gulp-puml');
+var cache = require('gulp-cached')
 var src = 'src/puml/**/*.puml';
 var target = './';
 
 gulp.task('puml', function () {
 	return gulp.src(src)
+		.pipe(cache('puml'))
 		.pipe(puml({format: 'svg'}))
 		.pipe(gulp.dest(target));
 });
