437f835120433999a1b0e80d1ba23584bb306200
ryuring <ryuring55@gmail.com>
2022-09-09 11:58:43 +0900

    gulp の処理を調整

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,86 @@
 const gulp = require('gulp');
 const puml = require('gulp-puml');
-const cache = require('gulp-cached');
 const browserSync = require('browser-sync').create();
-const [target, src] = ['./', 'src/puml/**/*.puml'];
-const runSequence = require('gulp4-run-sequence');
-const gulpif = require('gulp-if');
 const exec = require('child_process').exec;
-const analogPuml = [
+
+/**
+ * ソースファイル
+ * @type {string}
+ */
+const src = 'src/puml/**/*.puml';
+
+/**
+ * コンパイル先
+ * @type {string}
+ */
+const target = './';
+
+/**
+ * Java実行対象ファイル 
+ * 行数が多いことにより、gulp-puml がうまくいかない場合、java側で実行する
+ * @see https://baserproject.github.io/5/ucmitz/etc/troubleshooting#gulp-pumlにてplantumlのコンパイルが失敗する場合
+ * @type {string[]}
+ */
+const pumlAnalogTargets = [
 	"src/puml/5/ucmitz/svg/class/manage_contents.puml",
 	"src/puml/5/ucmitz/svg/class/manage_sites.puml",
 	"src/puml/5/ucmitz/svg/class/manage_content_folders.puml",
 ];
-gulp.task('browser-sync', function() {
+
+/**
+ * browser-sync を初期化
+ */
+function initBs() {
     browserSync.init({
 		proxy: 'localhost:4000'
     });
-});
+}
 
-gulp.task('bs-reload', function(){
+/**
+ * ブラウザをリロード
+ */
+function bsRload(){
     browserSync.reload();
-});
+}
 
-gulp.task('puml', function () {
-	// 初回時に全データを吐き出さないようにキャッシュされたかを確認
-	const isCached = cache.caches['puml'] !== undefined;
-	return gulp.src(src)
-		.pipe(cache('puml'))
-		.pipe(puml({format: 'svg'}))
-		.pipe(gulpif(isCached, gulp.dest(target)));
-});
-
-gulp.task('watch', function() {
-	return gulp.watch(src, ['default']);
-});
-
-gulp.task('default', gulp.series(
-	gulp.parallel('browser-sync', 'puml', function () {
-		gulp.watch(src).on("change", function ($file) {
-			// 行数が多いことにより、gulpがうまくいかないためjava側で実行
-			/** @see https://baserproject.github.io/5/ucmitz/etc/troubleshooting#gulp-pumlにてplantumlのコンパイルが失敗する場合 */
-			if (analogPuml.includes($file)) {
-				path = $file.replace('src/puml/', '');
-				fileName = path.split('\/').pop();
-				reg = new RegExp('\/' + fileName.replace('.', '\.'), 'g');
-				targetDir = '../../../../../../' + path.replace(reg, '');
-				exec(`java -jar plantuml.jar -verbose -o "${targetDir}"  ${$file}  -tsvg`,
-					function (error) {
-						if (error !== null) {
-							console.log('exec error: ' + error);
-						}
-					}
-				);
-			} else {
-				return runSequence(
-					'puml',
-					'bs-reload'
-				);
+/**
+ * PlantUML を書き出す
+ * @returns {*}
+ */
+function buildPuml(file) {
+	if (pumlAnalogTargets.includes(file)) {
+		path = file.replace('src/puml/', '');
+		fileName = path.split('\/').pop();
+		reg = new RegExp('\/' + fileName.replace('.', '\.'), 'g');
+		targetDir = '../../../../../../' + path.replace(reg, '');
+		exec(`java -jar plantuml.jar -verbose -o "${targetDir}"  ${$file}  -tsvg`,
+			function (error) {
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
 			}
-		});
-	})
-));
+		);
+	} else {
+		// ファイル単体で引き渡す
+		return gulp.src([file], { base: './src/puml' })
+			.pipe(puml({format: 'svg'}))
+			.pipe(gulp.dest(target));
+	}
+	gulp.series(bsRload);
+}
+
+/**
+ * ファイルを監視する
+ */
+function watchFiles() {
+	gulp.watch(src).on('change', function(file){
+		console.log(file);
+		buildPuml(file);
+	});
+}
+
+/**
+ * 処理実行
+ */
+gulp.task('default', gulp.parallel(initBs, watchFiles));
+
